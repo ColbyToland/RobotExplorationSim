1d3fb4ab9a810b3b3d56a7dd03bc3f95e2b7421b
Colby Toland <colby.toland@gmail.com>
2024-08-04 16:01:32 -0600

    Add pytest and enable command line config filename passing

--- explorer.py
+++ explorer.py
@@ -1,16 +1,20 @@
-"""
-Heavily based on the demo code. This has a procedurally generated map that is randomly explored by a robot using A*
-"""
-
 import arcade
+import argparse
 
 from ExplorerConfig import ExplorerConfig
 from MainMenuView import MainMenuView
 
+BASE_NAME = "Arcade Explorer"
 
 def main():
-    window_config = ExplorerConfig().window_settings()
-    window = arcade.Window(window_config['width'], window_config['height'], "Arcade Explorer" + window_config['subtitle'], resizable=True)
+    parser = argparse.ArgumentParser(prog="Arcade Explorer",
+                                     description="Simulate robots traversing a map with sensors.")
+
+    parser.add_argument('config_filename')
+    args = parser.parse_args()
+
+    window_config = ExplorerConfig(args.config_filename).window_settings()
+    window = arcade.Window(window_config['width'], window_config['height'], BASE_NAME + window_config['subtitle'], resizable=True)
     start_view = MainMenuView()
     window.show_view(start_view)
     arcade.run()
